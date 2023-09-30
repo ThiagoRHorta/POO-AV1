3ab8a74d4ad81ca49e28911c0f43eb5253dfb991
ThiagoRHorta <83096373+ThiagoRHorta@users.noreply.github.com>
2023-09-30 18:44:05 -0300

    Mudança na leitura do arquivo
    Todos users cadastrados em credenciais.txt serão inicializados antes de abrir a janela

--- AV1/src/main/java/com/mycompany/av1/AV1.java
+++ AV1/src/main/java/com/mycompany/av1/AV1.java
@@ -7,33 +7,31 @@ import java.io.IOException;
 
 public class AV1 {
     
-    private static void criarAdmin(){
+    private static void inicializarUsuarios() {
         String fileName = "credenciais.txt";
-        String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\com\\mycompany\\av1\\";
-        String file = filePath+fileName;
+        String filePath = System.getProperty("user.dir") + "\\src\\main\\java\\com\\mycompany\\av1\\";
+        String file = filePath + fileName;
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line = br.readLine();
+            String linha;
 
-            if (line != null) {
-                String[] values = line.split(",");
+            while ((linha = br.readLine()) != null) {
+                String[] values = linha.split(",");
                 if (values.length == 4) {
                     Usuario usuario = new Usuario(values[0], values[1], values[2], TipoUsuario.valueOf(values[3]));
                     System.out.println(usuario);
                 } else {
                     System.out.println("Formato de linha inválido no arquivo. Favor seguir com 'nome,usuario,senha,TipoUsuario'");
                 }
-            } else {
-                System.out.println("O arquivo está vazio.");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }   
+    } 
     
     public static void main(String[] args) {
         //Inicializar o programa com um login de adm
-        criarAdmin();
+        inicializarUsuarios();
         
         //Iniciar tela
         MenuPrincipal.main(args);
